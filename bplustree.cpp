37a83726ce850af48e2110ae996ba168bf406430
kjurczyk <kjurczyk@ufl.edu>
2021-03-28 00:03:20 -0400

    started reading in file

--- bplustree.cpp
+++ bplustree.cpp
@@ -8,9 +8,11 @@
 // For Java:
 // $ java bplustree file_name
 // Where file_name is the name of the file that has the input test data.
+// argc = # of arguments
+// argv[i] allows you to access the arguments from the command line
 ////////////////////////////////////////////////
 
-int main()
+int main(int argc, char ** argv)
 {
   // create a string to hold the filename
   string file_name = "";
@@ -20,10 +22,18 @@ int main()
 	std::cin >> file_name;
   std::cout << "file_name is " + file_name;
 
-  // read in what's in the file called file_name
+  //check that the file is openable
+	if(fileCanBeOpened(file_name))
+	{
+		return 1;	//the program will die if you don't enter a valid file name
+	}
+
   // create a 1D array into which all of the different instructions can be inserted
   // make sure to delete the array afterwards
   vector<string> textInFile;
+
+  // read in what's in the file called file_name into the vector
+  textInFile = readFile(file_name);
 
   // at the end of the program
   return 0;
--- bplustree.h
+++ bplustree.h
@@ -13,3 +13,71 @@ class Pair
   int key;
   float value;
 };
+
+//////////////////////////////////////////////////////////////
+// checks that the file is valid - if not, it will return 1 and print 
+// out that the file was not opened. The 1 will make the program die.
+//////////////////////////////////////////////////////////////
+bool fileCanBeOpened(string name)	
+{
+	std::ifstream file;
+	file.open(name.c_str());
+
+  // if the file does not open
+	if (!file.is_open())
+	{
+		cerr << "File not opened" << endl;
+		return false;
+	}
+
+  // otherwise, it correctly opened
+	return true;
+}
+
+///////////////////////////////////////////////////////////////
+// reads in what is in the file into a vector<string>
+// returns the vector<string>
+// each new line in the file gets its own unit in the vector
+///////////////////////////////////////////////////////////////
+vector<string> readFile(string name)
+{
+  std::ifstream file;
+  string lineOfInput = "";
+  vector<string> toReturn;
+
+  // open the file
+  file.open(name.c_str());
+
+  // check if the file actually opened correctly
+  if (file.is_open())
+	{
+    // getline reads one line of the file and puts it in the lineOfInput string
+		while(getline(file, lineOfInput))
+    {
+      toReturn.push_back(lineOfInput);  // append line to the vector
+      std::cout << lineOfInput << endl; // print line out for debugging
+    }
+      // close the file
+      file.close();
+	}
+  else  // if the file was not able to be opened
+  {
+    cerr << "File not opened" << endl;
+  }
+  return toReturn;
+
+}
+
+//////////////////////////////////////////////////////////
+// gets input from the user and places it into the array in 
+// the order that the user input it
+//////////////////////////////////////////////////////////
+void takeInput(int * arr, int givenL)
+{
+	int input = 0;
+	for(int i = 0; i < givenL; i++)	//it knows how many elements are supposed to be in the array and makes sure that many are entered
+	{
+		std::cin >> input;
+		arr[i] = input;
+	}
+}
