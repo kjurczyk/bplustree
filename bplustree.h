fe758c7d6dc6c35ee8ed645b00515b468394fd8f
kjurczyk <kjurczyk@ufl.edu>
2021-04-02 13:00:55 -0400

    started splitting operation for root nodes

--- bplustree.h
+++ bplustree.h
@@ -5,6 +5,8 @@
 using std::string;
 using namespace std;
 #include <math.h>
+#include <algorithm>
+#include <array>
 
 // Now to insert some variables that will always be accessible
 // global variables will be listed with g_ in front of name
@@ -25,6 +27,12 @@ class Pair
   {
     key = k;
     value = v;
+  }
+
+  Pair()
+  {
+    key = 0;
+    value = 0;
   }
 };
 
@@ -102,8 +110,8 @@ class BPTree
       nodeObject *prevLeaf = nullptr; // holds a pointer to the previous leaf, if this is a leaf node - otherwise we won't use this
       nodeObject *nextLeaf = nullptr; // holds a pointer to the next leaf, if this is a leaf node - otherwise we won't use it
       // we also won't know until later how many keys will be stored in here
-      vector<int> keys; // should be empty if this is a leaf node
-      vector<Pair> keyValues; // should be empty if this is an index node
+      int * keys; // should be empty if this is a leaf node
+      Pair * keyValues; // should be empty if this is an index node
       
       // we won't know until later how many children there will be, so make an array of child pointers
       nodeObject *children[]; // holds pointers to the children - set it to null when you can
@@ -156,12 +164,14 @@ class BPTree
   nodePointer findSpot(int key)
   {
     nodePointer seeker = rootNode;  // start at the root node and work down
+    seeker->keys[0];
     // is this a leaf node?
     if(seeker->typeOfNode == LEAF)
     {
       // we can enter the data here
-      
     }
+    if(rootNode == nullptr)
+      std::cout << "findspot: root node was null" << endl;
     
     return seeker;
   }
@@ -170,30 +180,114 @@ class BPTree
   // might need to split the leaf to make room
   void addNode(nodePointer spot, int key, float value)
   {
+    Pair * p = new Pair(key, value);
     // can we place the key into this specific node?
-      if(spot->numKeys + 1 <= maxKeys)
+      if(spot->numKeys < maxKeys + 1)
       {
+      cout<<"if statement entered" <<endl;
         // find correct location in array to insert the key
-        int location = 0;
-        for(int i = 0; i < maxKeys; i++)
+        int location = spot->numKeys; // start with the last possible position in the array
+        for(int i = spot->numKeys - 1; i >= 0; i--)
         {
-          if(spot->keyValues[i].key > key)
+          if(spot->keys[i] > key)  // find the correct location and move anything bigger backwards
           {
             location = i;
+            spot->keys[i+1] = spot->keys[i];  // move it over by 1 so that the i spot can be overwritten
+            spot->keyValues[i+1] = spot->keyValues[i];
+          }
+          else  // we've reached the smallest location 
+          {  
             break;
           }
         }
+        // insert into the appropriate location in the array
+          spot->keys[location] = key;
+          cout << "added key: " << spot->keys[location] << " at location: " << location << endl;
+          spot->keyValues[location] = * new Pair(key, value);
+          spot->numKeys = spot->numKeys+1;
+          std::cout << "\tnumber of keys: " << spot->numKeys << endl;
+          //std::cout << "/tthe number of keys in this node is " + spot->numKeys << " test" << endl;
+        
         // std::vector<Pair>::iterator it = spot->keyValues.begin();
         // it = it + location;
-        Pair * p = new Pair(key, value);
+        
         // insert into the key & value arrays
         //insertKeyValueInLocation(key, spot->keyValues, location);
         //spot->keyValues.insert(spot->keyValues.begin() + location, new Pair(key, value));
-        spot->keyValues.push_back(*p);
+        
+        /*spot->keyValues[spot->numKeys] = *p;
+        spot->keys[spot->numKeys] = key;
+        sort(*spot->keys,spot->numKeys+1);*/
+        
+        printNodeKeys(spot->numKeys, spot->keys);
+        
         
         //insertValueInLocation(value, spot->values, location);
-        //AM I ALLOWED TO USE VECTORS????
+        
       }
+      // we have added the key and value
+      // now check if what we did was legal. If not, fix it
+      if(spot->numKeys > maxKeys)
+      {
+        cout << "this was an illegal add. We need to fix it" << endl;
+        // break the leaf node apart
+        // determine where to split it
+        int smallestLargeSpot = spot->numKeys/2;
+        nodePointer smallNode = getSmallestNode(spot->keyValues, smallestLargeSpot);
+        cout<< "successfully created a small node:" << endl;
+        printNodeKeys(smallNode->numKeys, smallNode->keys);
+        nodePointer largeNode = getLargestNode(spot->keyValues, smallestLargeSpot, spot->numKeys);
+        cout<< "successfully created a large node:" << endl;
+        printNodeKeys(largeNode->numKeys, largeNode->keys);
+
+      }
+  }
+
+  void printNodeKeys(int numKeys, int keys[])
+  {
+    cout << "\tnumKeys: " << numKeys << endl;
+    for (int i = 0; i < numKeys; i++)
+    {
+      cout << "\t\t" << keys[i] << endl;
+    }
+  }
+
+  nodePointer setUpNode(int numKeys)
+  {
+    nodePointer toReturn = new nodeObject();
+    toReturn->children[maxKids];
+    toReturn->numKeys = numKeys;
+    toReturn->keys = new int[maxNodes];
+    toReturn->keyValues = new Pair[maxKeys + 1];
+    return toReturn;
+  }
+
+  nodePointer getSmallestNode(Pair kv[], int smallestLargeSpot)
+  {
+    nodePointer toReturn = setUpNode(smallestLargeSpot);    
+
+    for(int i = 0; i < smallestLargeSpot; i++)
+    {
+      toReturn->keys[i] = kv[i].key;
+      toReturn->keyValues[i] = kv[i];
+      //cout << toReturn->keys[i] << endl;
+    }
+    return toReturn;
+  }
+
+  nodePointer getLargestNode(Pair kv[], int smallestLargeSpot, int numKeys)
+  {
+    nodePointer toReturn = setUpNode(numKeys - smallestLargeSpot);    
+
+    int pos = 0;
+    for(int i = 0; i + smallestLargeSpot< numKeys; i++)
+    {
+      toReturn->keys[i] = kv[i + smallestLargeSpot].key;
+      //cout << "\tjust added: " << toReturn->keys[i] << " in pos " << pos << endl;;
+      toReturn->keyValues[i] = kv[i + smallestLargeSpot];
+      pos++;
+    }
+    return toReturn;
   }
 
   void insert(int key, float value)
@@ -202,16 +296,23 @@ class BPTree
     // if this is the root node
     if(spot == NULL)  // this is the first node, so the root pointer points here
     {
-      nodePointer rootNode = new nodeObject();
+      rootNode = new nodeObject();
       rootNode->typeOfNode = LEAF;  // this is the root, but it's also a leaf - leaf wins out at beginning
       //rootNode->keys[maxKeys + 1]; // we don't want to get to the +1 keys, but we need it for a 
       // short time to store the data before we can split the node
       rootNode->children[maxKids];
-      rootNode->numKeys = 0;
-      //rootNode->keyValues[maxKeys + 1];
+      rootNode->numKeys = 1;
+      rootNode->keys = new int[maxNodes];
+      rootNode->keys[0] = key;
+      rootNode->keyValues = new Pair[maxKeys + 1];
+      rootNode->keyValues[0] = * new Pair(key, value);
+      std::cout << "root is: " << rootNode->keys[0] << endl;
+      std::cout << "number of keys is: " << rootNode->numKeys << endl;
+            std::cout << " " << endl;
     }
     else  // we already have a root node so add another BPTree which has a max of maxNodes and is not the root (yet)
     {
+      std::cout << "root node was not null, so I am adding a node" << endl;
       // find the correct node to add this to the tree
       addNode(spot, key, value);
       // add a data node in the correct spot
